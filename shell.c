b57192fcfa488b9a2d5c1d792f85aec013c346c1
Nick <ngg3vm@virginia.edu>
2015-02-01 21:04:44 -0500

    Input is now tokenized.

--- shell.c
+++ shell.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main() {
 	// Adjustable input line length
 	int input_line_length = 100;
 	char input[input_line_length]; // 100 as per shell specs. This can be upped if necessary.
+	char **arguments; // My super janky vector
+	arguments = malloc(0);
 	// Main shell loop
 	do {
 		// Read in a line.
@@ -17,8 +21,32 @@ int main() {
 			while ((c = fgetc(stdin)) != EOF && c != '\n');
 		}
 		else {
-			printf("input: %s", input);
+			int word_size = 0;
+			char *tok;
+			bool to_free = false;
+			tok = strtok(input, " ");
+			while (tok) {
+				printf("Token: %s\n", tok);
+				// We've found our word
+				//if (strcmp(tok, "|") && strcmp(tok, ">") && strcmp(tok, "<")) {
+				//	printf("blah\n");
+				//}
+				// Add another element to arguments
+				arguments = realloc(arguments, sizeof(char*) * (word_size + 1));
+				// Adding tok to the end of arguments
+				arguments[word_size] = malloc(sizeof(char) * strlen(tok));
+				to_free = true;
+				strcpy(arguments[word_size], tok);
+				word_size++;
+				// Finding next token
+				tok = strtok(NULL, " ");
+			}
+			if (to_free) {
+				free(arguments);
+			}
 		}
-	} while (strcmp(input, "exit\n")  != 0); // Exit command
+		// Example ls. Note the final NULL argument and the duplicated binary path
+		//execl("/bin/ls", "/bin/ls", "/home/nick/MyFirstShell", NULL);
+	} while (strcmp(input, "exit\n")); // Exit command
 	return 0;
 }
